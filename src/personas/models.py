79a74b9c1f5962e8803462127c771df4e8e11998
Omar Cristhian Romero Chipana <oromero@unsa.edu.pe>
2022-06-14 12:30:45 -0500

    actualizando los campos antiguos

--- src/personas/models.py
+++ src/personas/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Persona(models.Model):
-    nombres=models.CharField(max_length=100)
-    apellidos=models.CharField(max_length=100)
+    nombres=models.CharField(max_length=100, null=True)
+    apellidos=models.CharField(max_length=100, blank=True)
     edad=models.IntegerField()
-    donador=models.BooleanField()
+    donador=models.BooleanField(default=False)
