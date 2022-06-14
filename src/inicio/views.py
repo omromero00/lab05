e58d252cabd516af2295ffd9bcde892727693fa9
Omar Cristhian Romero Chipana <oromero@unsa.edu.pe>
2022-06-14 13:22:56 -0500

    agregando variables al contexto

--- src/inicio/views.py
+++ src/inicio/views.py
@@ -6,7 +6,11 @@ from django.http import HttpResponse
 
 def miHomeView(request, *args, **kwargs):
     print(request.user)
-    return render(request, "home.html", {})
+    micontexto={
+        "mitexto": "hola texto",
+        "minumero": 123,
+    }
+    return render(request, "home.html", micontexto)
 
 def otraVista(*args, **kwargs):
     return HttpResponse("<h1> Mi otra vista </h1>")
