46ea33f5061590fcd8c1a8bdb8fab6aec4904124
Omar Cristhian Romero Chipana <oromero@unsa.edu.pe>
2022-06-22 17:11:46 -0500

    ejemplo de formulario con el objeto persona

---
+++ src/personas/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from .models import Persona
+class PersonaForm(forms.ModelForm):
+  class Meta:
+    model=Persona
+    fields=[
+        'nombres',
+        'apellidos',
+        'edad',
+        'donador',
+        ]
--- src/personas/views.py
+++ src/personas/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Persona
+from .forms import PersonaForm
+
 def personaTestView(request):
   obj=Persona.objects.get(id=1)
   context={
@@ -8,3 +10,13 @@ def personaTestView(request):
 
       }
   return render(request,'personas/descripcion.html',context)
+
+def personaCreateView(request):
+  form=PersonaForm(request.POST or None)
+  if form.is_valid():
+    form.save()
+    form=PersonaForm()
+  context={
+      'form':form
+      }
+  return render(request,'personas/personasCreate.html',context)
